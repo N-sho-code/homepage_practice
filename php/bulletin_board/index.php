87eecff4985927e9650df3b588b99378f4f66b0a
N-sho-code <shou1408829@yahoo.co.jp>
2025-06-24 09:02:56 +0900

    Update index.php

--- php/bulletin_board/index.php
+++ php/bulletin_board/index.php
@@ -1,12 +1,28 @@
 <?php 
+
+    $comment_array = array();
+
+    header('Content-Type: text/html; charset=UTF-8'); // ← 必ず最上部に追加
     //書き込みボタンが押されたか？
     if(!empty($_POST["submitButton"])){
         echo $_POST["username"];
         echo $_POST["comment"];
     }
-   //
-?>
+   //DB接続
+   try{
+    $pdo=new PDO('mysql:host=localhost;port=8889;dbname=bulletin_board',"root","root");
+   }catch(PDOException $e){
+    echo $e->getMessage();
+    /*error_log($e->getMessage()); // ← ログに出力
+    echo 'データベース接続に失敗しました。'; // ← 画面には固定メッセージ*/
+   }
+   //DBからコメントデータを取得する
+   $sql = "SELECT `id`, `username`, `comment`, `postDate` FROM `bulletin_board_table`;";
+   $comment_array = $pdo->query($sql);
 
+   //DBの接続を閉じる
+   $pdo = null;
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -23,16 +39,18 @@
     <main>
     <div class="boardWrapeer">
         <section>
-            <article>
-                <div class="wrapper">
-                    <div class="nameArea">
-                        <span>名前：</span>
-                        <p class="username">shinCode</p>
-                        <time>2025/06/06<time>
+            <?php foreach ($comment_array as $comment) :?>
+                <article>
+                    <div class="wrapper">
+                        <div class="nameArea">
+                            <span>名前：</span>
+                            <p class="username"><?php echo $comment["username"];?></p>
+                            <time><?php echo $comment["postDat"];?><time>
+                        </div>
+                        <p class="comment"><?php echo $comment["comment"];?></p>
                     </div>
-                    <p class="comment">手書きコメント</p>
-                </div>
-            </article>
+                </article>
+            <?php endforeach; ?>
         </section>
         <form class="formWrapper" method="POST">
             <div>
