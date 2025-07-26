85799f3c0ae221f0c3eef62d26e2440ae0c2669e
N-sho-code <shou1408829@yahoo.co.jp>
2025-07-26 11:17:13 +0900

    Update index.php

--- php/quiz-app/index.php
+++ php/quiz-app/index.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>クイズスタート</title>
+    <title>クイズに挑戦！</title>
 </head>
 <body>
     <main>
         <form action="quiz.php" method="post">
             <input type="hidden" name="q" value="0">
             <input type="hidden" name="score" value="0">
-            <button type="submit">クイズを始める</button>
+            <button type="submit">始める</button>
         </form>
     </main>
 </body>
