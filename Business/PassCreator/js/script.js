2f6ef7605a729c2946d459e73001da0da7547eb1
N-sho-code <shou1408829@yahoo.co.jp>
2026-05-13 22:25:52 +0900

    一部追加

---
+++ Business/PassCreator/js/script.js
@@ -0,0 +1,7 @@
+function generatePasswords(){
+    //画面に入力された値の取得
+    const lenght = document.getElementById('lenght').value;
+    const count = document.getElementById('count').value;
+    const excludeAmbiguous = document.getElementById("excludeAmbiguous").checked;
+    const excludeUppercase = document.getElementById("excludeUppercase").checked;
+}
